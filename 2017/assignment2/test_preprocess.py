24fb646f2fb1dff9da533e1ba7b1209344ec3f9f
Ramon Iglesias <rdit@stanford.edu>
2017-10-31 13:55:49 -0700

    sparse

--- 2017/assignment2/test_preprocess.py
+++ 2017/assignment2/test_preprocess.py
@@ -8,4 +8,6 @@ D = np.zeros((2, a.shape[0], a.shape[1], a.shape[2]))
 D[0,:,:,:] = a
 D[1,:,:,:] = a
 
-preprocess(D)
+data_l, gt_ab_313, prior_boost_nongray = preprocess(D)
+
+print np.nan_to_num(gt_ab_313)[0,:,:,:].sum(axis=-1).shape
