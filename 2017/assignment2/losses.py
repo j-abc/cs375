bba8a3c4d4319acd6498c5cb558af08f10042d3e
Ramon Iglesias <rdit@stanford.edu>
2017-10-30 22:59:13 -0700

    diana

--- 2017/assignment2/losses.py
+++ 2017/assignment2/losses.py
@@ -9,6 +9,7 @@ def colorful_loss(inputs, outputs, **target_params):
     flat_pred = tf.reshape(outputs['pred'], [-1, 313])
     flat_gt_ab_313 = tf.reshape(outputs['gt_ab_313'], [-1,313])
     loss = tf.reduce_sum(tf.nn.softmax_cross_entropy_with_logits(logits=flat_pred, labels=flat_gt_ab_313))
+    print target_params
     return loss / target_params['batch_size']
 
 def vae_loss(inputs, outputs):
