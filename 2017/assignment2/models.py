865f9e92f50be864fa15084a06c109de732c5cd4
Ramon Iglesias <rdit@stanford.edu>
2017-11-01 00:06:06 -0700

    asdf

--- 2017/assignment2/models.py
+++ 2017/assignment2/models.py
@@ -13,111 +13,6 @@ variational
 - add the colorization model
 '''
 
-# autoencoder
-def autoencoder(input_shape=[None, 784],
-                n_filters=[1, 10, 10, 10],
-                filter_sizes=[3, 3, 3, 3],
-                corruption=False):
-    """Build a deep denoising autoencoder w/ tied weights.
-    Parameters
-    ----------
-    input_shape : list, optional
-        Description
-    n_filters : list, optional
-        Description
-    filter_sizes : list, optional
-        Description
-    Returns
-    -------
-    x : Tensor
-        Input placeholder to the network
-    z : Tensor
-        Inner-most latent representation
-    y : Tensor
-        Output reconstruction of the input
-    cost : Tensor
-        Overall cost to use for training
-    Raises
-    ------
-    ValueError
-        Description
-    """
-    # %%
-    # input to the network
-    x = tf.placeholder(
-        tf.float32, input_shape, name='x')
-
-
-    # %%
-    # TODO: this need not be 2d
-    # ensure 2-d is converted to square tensor.
-    if len(x.get_shape()) == 2:
-        x_dim = np.sqrt(x.get_shape().as_list()[1])
-        if x_dim != int(x_dim):
-            raise ValueError('Unsupported input dimensions')
-        x_dim = int(x_dim)
-        x_tensor = tf.reshape(
-            x, [-1, x_dim, x_dim, n_filters[0]])
-    elif len(x.get_shape()) == 4:
-        x_tensor = x
-    else:
-        raise ValueError('Unsupported input dimensions')
-    current_input = x_tensor
-
-    # %%
-    # Optionally apply denoising autoencoder
-    if corruption:
-        current_input = corrupt(current_input)
-
-    # %%
-    # Build the encoder
-    encoder = []
-    shapes = []
-    for layer_i, n_output in enumerate(n_filters[1:]):
-        n_input = current_input.get_shape().as_list()[3]
-        shapes.append(current_input.get_shape().as_list())
-        W = tf.Variable(
-            tf.random_uniform([
-                filter_sizes[layer_i],
-                filter_sizes[layer_i],
-                n_input, n_output],
-                -1.0 / math.sqrt(n_input),
-                1.0 / math.sqrt(n_input)))
-        b = tf.Variable(tf.zeros([n_output]))
-        encoder.append(W)
-        output = lrelu(
-            tf.add(tf.nn.conv2d(
-                current_input, W, strides=[1, 2, 2, 1], padding='SAME'), b))
-        current_input = output
-
-    # %%
-    # store the latent representation
-    z = current_input
-    encoder.reverse()
-    shapes.reverse()
-
-    # %%
-    # Build the decoder using the same weights
-    for layer_i, shape in enumerate(shapes):
-        W = encoder[layer_i]
-        b = tf.Variable(tf.zeros([W.get_shape().as_list()[2]]))
-        output = lrelu(tf.add(
-            tf.nn.conv2d_transpose(
-                current_input, W,
-                tf.stack([tf.shape(x)[0], shape[1], shape[2], shape[3]]),
-                strides=[1, 2, 2, 1], padding='SAME'), b))
-        current_input = output
-
-    # %%
-    # now have the reconstruction through the network
-    y = current_input
-    # cost function measures pixel-wise difference
-    cost = tf.reduce_sum(tf.square(y - x_tensor))
-
-    # %%
-    return {'x': x, 'z': z, 'y': y, 'cost': cost}
-
-
 # variational autoencoder
 def vae_model(inputs, train=True, norm=True, **kwargs):
     # create outputs
@@ -136,7 +31,7 @@ def vae_model(inputs, train=True, norm=True, **kwargs):
     decoding_channels = [64, 32, in_shp[-1]]
     decoding_bns = [True, False, True]
 
-    zdim = 10
+    zdim = 20
     weight_decay = 1e-3
     dropout = .5 if train else None
     # encoding layers
