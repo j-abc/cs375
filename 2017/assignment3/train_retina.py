6498efe4b07896311977541e153356d4c05ea037
Ramon Iglesias <rdit@stanford.edu>
2017-11-12 15:38:59 -0800

    tfrecords

--- 2017/assignment3/train_retina.py
+++ 2017/assignment3/train_retina.py
@@ -19,7 +19,7 @@ stim_type = 'whitenoise'
 # Figure out the hostname
 host = os.uname()[1]
 #if 'neuroaicluster' in host:
-if True # the line above seemed useless
+if True: # the line above seemed useless
     if train_net:
         print('In train mode...')
         TOTAL_BATCH_SIZE = 5000
