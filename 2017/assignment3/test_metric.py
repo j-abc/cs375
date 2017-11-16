51aab87439a8c194f762df5b0506288b4e93516f
Ramon Iglesias <rdit@stanford.edu>
2017-11-15 16:48:59 -0800

    Automatic Commit. Pushing to server.

--- 2017/assignment3/test_metric.py
+++ 2017/assignment3/test_metric.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 from deepretina.metrics import cc
+import numpy as np
 
 A = tf.constant([
 	[1.,2.,3.],
@@ -10,7 +11,19 @@ B = tf.constant([
 	[3.,4.5,5.5]
 	])
 
+D = np.array([
+	[1.,2.,3.],
+	[3.,4.,5.]
+	])
+E = tf.constant([
+	[1.5,2.,3.5],
+	[3.,4.5,5.5]
+	])
+
+print cc(D,E)
+
 loss = tf.py_func(cc, [A, B], tf.float32)
+
 
 with tf.Session() as sess:
 	a = loss.eval()
