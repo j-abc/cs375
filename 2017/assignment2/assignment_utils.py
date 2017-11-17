748e1de0c8acb11cf82c914b9086084a1bb9d7c6
Ramon Iglesias <ramon.d.iglesias@gmail.com>
2017-11-17 07:58:31 +0000

    checkout

--- 2017/assignment2/assignment_utils.py
+++ 2017/assignment2/assignment_utils.py
@@ -8,16 +8,35 @@ import scipy.signal as signal
 import matplotlib.pyplot as plt
 from tqdm import tqdm_notebook, trange
 
+def get_neural_exp_data(coll, exp_id):
+    q_val = {'exp_id' : exp_id, 'validation_results' : {'$exists' : True}}
+    
+    # create our data dictionary
+    val_steps = coll.find(q_val, projection = ['validation_results'])
+    val_keys = {val_steps[i]['validation_results'].keys()[0]: val_steps[i]['validation_results'] for i in range(val_steps.count())}
+    val_keys = {ikey: item[ikey] for ikey, item in val_keys.iteritems()}
+    val_vals = [[ikey] + ikey.split('_') for ikey in val_keys.keys()]
+    ddict = {(ilist[3], int(ilist[2])):val_keys[ilist[0]]for ilist in val_vals}
+    
+    # get unique steps and var levels
+    import pandas as pd
+    key_df = pd.DataFrame(val_vals, columns = ['key', 'fill', 'step', 'v'])
+    uni_steps = key_df.step.unique().astype(int)
+    uni_var = key_df.v.unique()
+    
+    return ddict, uni_steps, uni_var
+
 def p_get_coll(collname, dbname):
     # connect to database
     dbname = dbname
     port = 24444
 
     conn = pm.MongoClient(port = port)
+    db = conn[dbname]
     coll = conn[dbname][collname + '.files']
 
     # print out saved experiments in collection coll
-    return coll
+    return coll, db
 
 def p22_training(exp_id, coll):
     def get_losses(exp_id):
@@ -55,16 +74,16 @@ def p22_training(exp_id, coll):
     plt.show()
     
 # Gets the validation data from the database
-def get_validation_data(exp_id, coll):
+def get_validation_data(exp_id, coll, num = 10):
 
     q_val = {'exp_id' : exp_id, 'validation_results' : {'$exists' : True}, 'validates' : {'$exists' : False}}
+
     val_steps = coll.find(q_val, projection = ['validation_results'])
-    l2_loss = [val_steps[i]['validation_results']['valid0']['l2_loss'] 
-        for i in range(0, val_steps.count(), 10)]
-    img_inputs = [val_steps[i]['validation_results']['valid0']['gt'] 
-        for i in range(0, val_steps.count(), 10)]
-    img_prediction = [val_steps[i]['validation_results']['valid0']['pred']
-        for i in range(0, val_steps.count(), 10)]
+    my_range = np.linspace(0, val_steps.count()-1, num).tolist()
+    my_range = set([int(round(i)) for i in my_range])
+    l2_loss = [val_steps[i]['validation_results']['valid0']['l2_loss'] for i in my_range]
+    img_inputs = [val_steps[i]['validation_results']['valid0']['gt'] for i in my_range]
+    img_prediction = [val_steps[i]['validation_results']['valid0']['pred'] for i in my_range]
     return l2_loss, img_inputs, img_prediction
 
 def plot_l2_loss(l2_loss, exp_id):
@@ -81,23 +100,33 @@ def plot_l2_loss(l2_loss, exp_id):
     
     plt.show()
 
-def p_get_data_list(coll, collname, step, v, idx = -1):
-    exp_id = 'experiment_1_%s_%s_%s'%(str(step), collname, v)
+
+    
+def p_get_data_list(coll, collname, step, v, exp_id, idx = -1):
+    #exp_id = 'experiment_1_%s_%s_%s'%(str(step), collname, v)
+
     def get_neural_validation_data(exp_id):
         q_val = {'exp_id' : exp_id, 'validation_results' : {'$exists' : True}, 'validates': {'$exists' : True}}
         val_steps = coll.find(q_val, projection = ['validation_results', 'validates', 'exp_id'])
-
         results = [val_steps[i] for i in range(val_steps.count())]
         for res in results:
             try:
                 res['step'] = coll.find({'_id': res['validates']})[0]['step']
             except:
                 res['step'] = -1
+        print(len(results))
         return results
-
+    
+    
     validation_data = get_neural_validation_data(exp_id=exp_id)
-    data = validation_data[idx]['validation_results']['valid0']
-    print validation_data[idx]['step'], v
+    all_keys = validation_data[idx]['validation_results'].keys()
+    data_all = {}
+    for key in all_keys:
+        print key
+        data = validation_data[idx]['validation_results'][key]
+        extraction_step = key.split('_')[1]
+        data_all[extraction_step] = data
+        print validation_data[idx]['step'], v
     return data
 
 def plot_rdms(data, target_layers, step):
@@ -160,31 +189,23 @@ def plot_categorization_results(data, target_layers, step, category=None):
 def plot_regression_results(data, target_layers, step):
     """
     Prints out the noise corrected multi rsquared loss for each layer.
+    
     You will need to EDIT this function.
     """
     for layer in target_layers:
-        try:
-            k = 'it_regression_%s' % layer
-            regression_results = data[k]
-            ### YOUR CODE HERE
-            calculated_regression_val = 1 - regression_results['noise_corrected_multi_rsquared_loss']
-            print('step: ' + str(step) + ", layer: " + layer + ", calculated_value: " + str(calculated_regression_val))
-        except:
-            print 'Oh no...' + k + ' did not regress'
-            pass
-        ### END OF YOUR CODE
+            print(data.keys())
+            k = 'it_regression_' + layer
+            print('step', step, 'layer', layer, 1 - data[k]['noise_corrected_multi_rsquared_loss'])
         
-def plot_conv1_kernels(data, step):
+def plot_conv1_kernels(data):
     """
     Plots the 96 conv1 kernels in a 12 x 8 subplot grid.
     
     You will need to EDIT this function.
     """
-    print('Iteration step: %d' % step)
     kernels = np.array(data['conv1_kernel'])
-    ### YOUR CODE HERE
-    x = 12
-    y = 8
+    x = 5
+    y = 6
     f, axarr = plt.subplots(x, y, figsize=(4*x, 4*y))
     for i in range(x):
         for j in range(y):
